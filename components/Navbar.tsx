c73ff4688ed0776286aa883abce64ab6664f2d65
Muhammad Ihsan <muhammadihsan076@gmail.com>
2026-02-15 08:51:25 +0700

    feat: make Navbar sticky and enhance its positioning for better visibility

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { TypographyH3 } from './Typography'
 
 const Navbar = () => {
   return (
-    <header className='flex items-center gap-3 h-16 px-4 bg-primary border-b border-border'>
+    <header className='sticky top-0 z-50 flex items-center gap-3 h-16 px-4 bg-primary border-b border-border'>
         <div className='relative h-10 w-10 shrink-0'>
           <Image src="/images/tj-putih.png" alt="logo" fill className='object-contain' />
         </div>
