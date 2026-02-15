d87642f1eaebc1dff3aefccd19d462d943e26907
Muhammad Ihsan <muhammadihsan076@gmail.com>
2026-02-15 20:55:21 +0700

    refactor: update vehicle card component to display last updated time in a more user-friendly format

--- components/cards/vehicle.tsx
+++ components/cards/vehicle.tsx
@@ -64,23 +64,33 @@ const VehicleCard = ({ vehicle }: { vehicle: VehicleWithRoute }) => {
     STATUS_CONFIG[attributes.current_status as VehicleCurrentStatus] ??
     DEFAULT_STATUS;
 
-  const updatedAt = useMemo(() => {
+//   const updatedAt = useMemo(() => {
+//     if (!attributes.updated_at) return null;
+
+//     const now = new Date();
+//     const date = new Date(attributes.updated_at);
+//     const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+//     if (diffInSeconds < 60) return 'Just now';
+
+//     const diffInMinutes = Math.floor(diffInSeconds / 60);
+//     if (diffInMinutes < 60) return `${diffInMinutes} minutes ago`;
+
+//     const diffInHours = Math.floor(diffInMinutes / 60);
+//     if (diffInHours < 24) return `${diffInHours} hours ago`;
+
+//     const diffInDays = Math.floor(diffInHours / 24);
+//     return `${diffInDays} days ago`;
+//   }, [attributes.updated_at]);
+
+  const updatedAtTimeOnly = useMemo(() => {
     if (!attributes.updated_at) return null;
-
-    const now = new Date();
     const date = new Date(attributes.updated_at);
-    const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
-
-    if (diffInSeconds < 60) return 'Just now';
-
-    const diffInMinutes = Math.floor(diffInSeconds / 60);
-    if (diffInMinutes < 60) return `${diffInMinutes} minutes ago`;
-
-    const diffInHours = Math.floor(diffInMinutes / 60);
-    if (diffInHours < 24) return `${diffInHours} hours ago`;
-
-    const diffInDays = Math.floor(diffInHours / 24);
-    return `${diffInDays} days ago`;
+    return date.toLocaleTimeString('id-ID', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    });
   }, [attributes.updated_at]);
 
   const occupancyStatus = (status: string) => {
@@ -162,10 +172,10 @@ const VehicleCard = ({ vehicle }: { vehicle: VehicleWithRoute }) => {
         </div>
 
         <div className="flex items-center justify-between text-xs text-muted-foreground/80">
-          {updatedAt && (
+          {updatedAtTimeOnly && (
             <div className="flex items-center gap-1">
               <Clock className="h-3 w-3" />
-              <span>{updatedAt}</span>
+              <span className="text-xs text-muted-foreground text-center">last updated at {updatedAtTimeOnly}</span>
             </div>
           )}
 
