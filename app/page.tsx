6527cb65a916a2e4deb38143aacfdd98fff21c4a
Muhammad Ihsan <muhammadihsan076@gmail.com>
2026-02-15 18:17:15 +0700

    feat: add TopContainer component for real-time dashboard and enhance vehicle list with pagination and select options

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import ContainerVehicleList from "./containers/VehicleList";
+import ContainerVehicleList from './containers/VehicleList';
+import TopContainer from './containers/TopContainer';
 
 export default function Home() {
-
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background">
+    <div className="flex flex-col min-h-screen bg-background">
+      <TopContainer />
       <ContainerVehicleList />
     </div>
   );
